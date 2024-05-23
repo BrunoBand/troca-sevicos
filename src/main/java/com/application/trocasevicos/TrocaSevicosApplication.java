deceadcc7c1416130a5b578c684f6a98c6a78ca3
Bruno Bandeira <brunorochaban@gmail.com>
2024-05-23 10:44:08 -0300

    initial commit

---
+++ src/main/java/com/application/trocasevicos/TrocaSevicosApplication.java
@@ -0,0 +1,14 @@
+package com.application.trocasevicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocaSevicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocaSevicosApplication.class, args);
+	}
+	
+
+}
